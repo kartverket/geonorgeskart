fb9ce5f35f13a90e9a74a94446f8540474924925
Dzenan Dumpor <Dzenan.Dumpor@geodata.no>
2017-11-13 16:21:51 +0100

    lage hook for å legge på ticket/token

--- widgets/NorgeDigitaltTicket/Widget.js
+++ widgets/NorgeDigitaltTicket/Widget.js
@@ -1,17 +1,16 @@
 define([
-    'dojo/_base/declare',
-    'dojo/_base/lang',
-    'dojo/_base/html',
-    'dojo/Deferred',
-    'jimu/BaseWidget',
-    'jimu/utils',
-    'jimu/portalUtils',
-    'jimu/dijit/Message',
-    "dojo/dom-style",
-    'dojo/query',
-    'dojo/NodeList-dom'
-  ],
-  function(
+  "dojo/_base/declare",
+  "dojo/_base/lang",
+  "dojo/_base/html",
+  "dojo/Deferred",
+  "jimu/BaseWidget",
+  "jimu/utils",
+  "jimu/portalUtils",
+  "jimu/dijit/Message",
+  "esri/arcgis/utils",
+  "esri/request"  
+],
+  function (
     declare,
     lang,
     html,
@@ -20,18 +19,59 @@ define([
     utils,
     PortalUtils,
     Message,
-    domStyle,
-    query) {
+    arcgisUtils,
+    esriRequest) {
     var clazz = declare([BaseWidget], {
       baseClass: "jimu-widget-norge-digitalt-ticket",
       name: "Norge Digitalt Ticket",
 
-      startup: function() {
+      postCreate: function () {
         this.inherited(arguments);
-        console.log("Hello from Norge Digitalt Ticket");
+        console.log("Hello from Norge Digitalt Ticket postCreate");
+
+        this.addUrlRewriterForAuthentication();
+
+        arcgisUtils.getItem(this.appConfig.map.itemId)
+          .then(function (response) {
+            var data = response.itemData;
+
+            var basemapLayers = data.baseMap.baseMapLayers;
+            var operationalLayers = data.operationalLayers;
+            var layers = basemapLayers.concat(operationalLayers);
+
+            console.log("All layers");
+            console.log(layers);
+
+            var wmtsLayers = layers.filter(function (l) { return l.type === "WebTiledLayer" });
+            var wmsLayers = layers.filter(function (l) { return l.type === "WMS" });
+          });
       },
 
-      onClose: function() {
+      addUrlRewriterForAuthentication: function() {
+        this.map.on("layer-add-result", function (evt) {
+          // TODO HOOK overwrite function getImageUrl getTileUrl
+        });
+      },
+
+      startup: function () {
+        this.inherited(arguments);        
+      },
+
+      getTicketForService: function (serviceId) {
+        var deferred = new Deferred();
+
+        esriRequest("https://localhost:44303/api/auth/ticket?serviceid=" + serviceId)
+          .then(function (ticket) {
+            deferred.resolve(ticket);
+          }, function (error) {
+            console.warn("Could not get token for service-id " + serviceId);
+            deferred.resolve(null);
+          });
+
+        return deferred.promise;
+      },
+
+      onClose: function () {
       }
 
     });
