d4752f42b8172a05dd7c394b04b5898f7be25fe4
Dzenan Dumpor <Dzenan.Dumpor@geodata.no>
2017-11-13 09:01:09 +0100

    adda nye widgets

---
+++ widgets/EditDrawnFeatures/Widget.js
@@ -0,0 +1,40 @@
+define([
+    'dojo/_base/declare',
+    'dojo/_base/lang',
+    'dojo/_base/html',
+    'dojo/Deferred',
+    'jimu/BaseWidget',
+    'jimu/utils',
+    'jimu/portalUtils',
+    'jimu/dijit/Message',
+    "dojo/dom-style",
+    'dojo/query',
+    'dojo/NodeList-dom'
+  ],
+  function(
+    declare,
+    lang,
+    html,
+    Deferred,
+    BaseWidget,
+    utils,
+    PortalUtils,
+    Message,
+    domStyle,
+    query) {
+    var clazz = declare([BaseWidget], {
+      baseClass: "jimu-widget-edit-drawn-features",
+      name: "Url Layer Controller",
+
+      startup: function() {
+        this.inherited(arguments);
+        console.log("Hello Edit Drawn Features");
+      },
+
+      onClose: function() {
+      }
+
+    });
+
+    return clazz;
+  });
