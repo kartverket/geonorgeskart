ad08e0772117a0b3c281f5e5ce96a345344c15ae
Henrik Schuller <hgs716@gmail.com>
2017-12-08 14:16:13 +0100

    Lagt inn litt config etc

---
+++ js/config_hgs_multilingual-layerlist.js
@@ -0,0 +1,39 @@
+var multi_layerList = [
+	{
+		"label": "Basis kartdata",
+		"label_en": "Basic mapstuff"
+	}, {
+		"label": "Energi",
+		"label_en": "Energy"
+	}, {
+		"label": "Forurensning",
+		"label_en": "Polution"
+	}, {
+		"label": "Friluftsliv",
+		"label_en": "outdoorstuff"
+	}, {
+		"label": "Geologi",
+		"label_en": "Geology"
+	}, {
+		"label": "Kulturminner",
+		"label_en": "Protected sites"
+	}, {
+		"label": "Kyst og fiskeri",
+		"label_en": "Coast and fishstuff"
+	}, {
+		"label": "Landbruk",
+		"label_en": "Landstuff"
+	}, {
+		"label": "Natur",
+		"label_en": "Nature"
+	}, {
+		"label": "Plan",
+		"label_en": "Plan"
+	}, {
+		"label": "Samferdsel",
+		"label_en": "Carstuff"
+	}, {
+		"label": "Samfunnssikkerhet",
+		"label_en": "Securitystuff"
+	}
+]
